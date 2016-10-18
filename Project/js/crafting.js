3f50364003592f9b7b3198b1ffe778e592e3153e
Server2016 <severinov.andrej@yandex.by>
2016-10-18 18:14:16 +0300

    Add files via upload

---
+++ Project/js/crafting.js
@@ -0,0 +1,59 @@
+
+var making =  (function () {
+
+    var _obj = ['Земля', 'Огонь','Воздух', 'Вода'];
+
+    var rec = [
+        {
+            in: ['Земля', 'Вода'],
+            out: 'Грязь'
+        },
+        {
+            in: ['Вода', 'Огонь'],
+            out: 'Пар'
+        },
+        {
+            in: ['Огонь', 'Земля'],
+            out: 'Лава'
+        },
+        {
+            in: ['Земля', 'Земля', 'Огонь'],
+            out: 'Стекло'
+        }
+
+    ];
+
+
+  
+
+    function doCraft (Items) {
+
+        var outputValue = '';
+        for (var i = 0; i < rec.length; i++) {
+
+            var selectedRule = rec[i].input.slice();
+            var numberOfTheSameItem = 0;
+
+            for (var j = 0; j < Items.length; j++) {
+
+                var index = selectedRule.indexOf(Items[j]);
+                if ( index >= 0 ) {
+                    selectedRule.splice(index, 1);
+                    numberOfTheSameItem++;
+                }
+            }
+
+            if (selectedRule.length === 0 && numberOfTheSameItem === Items.length) {
+                outputValue = rec[i].output;
+            }
+
+
+        }
+
+
+        return (outputValue.length !== 0) ? outputValue : 0;
+    }
+    return {
+        doCraft : doCraft
+    };
+})();
---
+++ Project/js/main.js
@@ -0,0 +1,109 @@
+
+var wel = ['Земля', 'Огонь','Воздух', 'Вода'];
+
+var ref = {
+    availableItems: document.getElementById('availableItems'),
+    craftingTable: document.getElementById('craftingTable'),
+    inputItem: document.getElementById('inputItem'),
+    add: document.getElementById('add'),
+    button: document.getElementById('button'),
+    result: document.getElementById('result'),
+    craftButton: document.getElementById('craftButton')
+
+
+};
+
+function app (value) {
+    wel.push(value);
+}
+function getItems () {
+    return wel;
+}
+var a = 0;
+
+function renderItems () {
+    items = wel;
+   ref.availableItems.innerHTML = '';
+    ref.add.value = '';
+    for (var i = 0; i < items.length; i++) {
+        putItem(items[i]);
+    }
+}
+renderItems();
+
+ref.button.addEventListener('click', addPart);
+ref.availableItems.addEventListener('drop', drop);
+ref.availableItems.addEventListener('dragover', dragOver);
+ref.craftingTable.addEventListener('drop', drop);
+ref.craftingTable.addEventListener('dragover', dragOver);
+ref.craftButton.addEventListener('click', craft);
+
+
+function putItem (itemName) {
+    var b = document.createElement('span');
+    b.id = 'el' + a;
+    b.className = 'element';
+    b.setAttribute('draggable', true);
+    b.ondragstart = drag;
+    b.innerHTML = itemName;
+    ref.availableItems.appendChild(b);
+    a++;
+}
+
+ref.button.addEventListener('click', addPart);
+
+function addPart () {
+    var value = ref.add.value;
+    if (value.length != 0) {
+        app(value);
+        renderItems();
+    }
+}
+function dragOver(ev){
+    ev.preventDefault();
+}
+function drag(ev){
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+function drop(ev){
+    ev.preventDefault();
+    var id = ev.dataTransfer.getData("text");
+    if (ev.target.id == 'availableItems' ||
+        ev.target.id == 'craftingTable') {
+        ev.target.appendChild(document.getElementById(id));
+    }
+
+}
+
+function craft () {
+    ref.result.innerHTML = '';
+
+    var Items = ref.craftingTable.querySelectorAll('span');
+
+    if (Items.length) {
+        var arrayItems = [];
+        
+        for (var i = 0; i < Items.length; i++) {
+            arrayItems.push(Items[i].innerHTML);
+        }
+
+        var outputResult = making.doCraft(arrayItems);
+
+        if (outputResult !== 0) {
+
+            app(outputResult);	
+            renderItems();	
+
+        } else {
+            ref.result.innerHTML = 'Неверный рецепт!';
+        }
+
+    }
+}
+
+
+document.getElementById('craftItems').addEventListener('click', toCraft);
+document.getElementById('wel').addEventListener('click', toCraft);
+document.getElementById('go').addEventListener('click', craft);
+document.getElementById('addBtn').addEventListener('click', addItem);
+itemsView();
